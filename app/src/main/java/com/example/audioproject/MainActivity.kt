342b1c16c906dc4159f09a6b342a96bd25c014ca
Ruon1s <50231327+Ruon1s@users.noreply.github.com>
2020-09-22 21:55:49 +0300

    fetching

--- app/src/main/java/com/example/audioproject/MainActivity.kt
+++ app/src/main/java/com/example/audioproject/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.audioproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.observe
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.results.observe(this, {Log.d("stuff", it.toString())})
+        viewModel.queryWithText("piano")
+
     }
 }
---
+++ app/src/main/java/com/example/audioproject/RetroFit.kt
@@ -0,0 +1,71 @@
+package com.example.audioproject
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.switchMap
+import kotlinx.coroutines.Dispatchers
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+object DemoApi {
+
+    private const val URL = "https://freesound.org/"
+    const val token = "TwC9eGABRWKuCNfmh7L0fd0mZbJSX0TlnXTu1NzX"
+    object Model {
+        data class Search(
+            val count: Int,
+            val next: String,
+            val previous: Any,
+            val results: List<Result>
+        )
+
+        data class Result(
+            val id: Int,
+            val license: String,
+            val name: String,
+            val tags: List<String>,
+            val username: String
+        )
+    }
+    interface Service {
+        @GET("apiv2/search/text/")
+        suspend fun getSounds(@Query("query") query: String,
+                              @Query("token") token: String
+        ) : Model.Search
+    }
+
+    private val loggingInterceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+                            .baseUrl(URL)
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .client(okHttpClient)
+                            .build()
+    val service = retrofit.create(Service::class.java)
+}
+
+class WebServiceRepository(){
+
+    private val call = DemoApi.service
+    suspend fun getSounds(query: String){ call.getSounds(query, DemoApi.token) }
+}
+
+class MainViewModel: ViewModel() {
+    private val repository: WebServiceRepository = WebServiceRepository()
+    val query = MutableLiveData<String>()
+    fun queryWithText(text: String) {query.value = text
+    }
+    val results = query.switchMap {
+        liveData(Dispatchers.IO) {emit(repository.getSounds(it))}
+    }
+}
