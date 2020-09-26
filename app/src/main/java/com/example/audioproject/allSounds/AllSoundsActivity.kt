501bd1cbb9ae14ca7296a5bc22812a721b38019b
Mikael Ruonala <Mikael.Ruonala@Metropolia.fi>
2020-09-26 08:51:31 +0300

    filetree

---
+++ app/src/main/java/com/example/audioproject/allSounds/AllSoundsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.audioproject.allSounds
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.audioproject.R
+
+class AllSoundsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_sounds)
+    }
+}
